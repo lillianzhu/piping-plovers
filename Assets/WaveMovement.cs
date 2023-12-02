e23afd3afd1bc321f7cb49357d12e4a0c927925a
Lillian Zhu <lillian@lilynow.com>
2023-12-02 12:29:50 -0800

    Water-maybe

---
+++ Assets/WaveMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    public float height = 0.05f;
+    public float startY = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        var pos = transform.position;
+        var newY = startY + height * Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(pos.x, newY, pos.z);
+    }
+}
